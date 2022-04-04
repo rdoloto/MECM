05662c487b32a02c2f574c8006190fe34fbd0265
Rafal Doloto <rdoloto@octane42.com>
2022-04-04 09:45:44 -0500

    Add files via upload

---
+++ Get-OfficeC2RCom.ps1
@@ -0,0 +1,26 @@
+﻿function Get-OfficeC2RCom{ 
+$comAdmin = New-Object -com ("COMAdmin.COMAdminCatalog")
+
+$applications = $comAdmin.GetCollection("Applications")
+$applications.Populate()
+
+foreach ($application in $applications)
+{
+ if ($application.Name -eq "OfficeC2RCom")
+ { $components = $applications.GetCollection("Components",$application.key) 
+  $components.Populate()
+  $componentscount= $components.Count
+
+if ($componentscount -eq 2){
+    write-host $false
+
+ 
+ }
+ if($componentscount -eq 1)
+ {   write-host $true}
+
+}
+ 
+}
+}
+Get-OfficeC2RCom
---
+++ Set-OfficeC2RCom.ps1
@@ -0,0 +1,31 @@
+﻿function Set-OfficeC2RCom{ 
+$comAdmin = New-Object -com ("COMAdmin.COMAdminCatalog")
+
+$applications = $comAdmin.GetCollection("Applications")
+$applications.Populate()
+
+foreach ($application in $applications)
+{
+ if ($application.Name -eq "OfficeC2RCom")
+ { $components = $applications.GetCollection("Components",$application.key) 
+  $components.Populate()
+
+ $comp=$components | Where-Object {$_.Name -eq "UpdateNotify.Object.2"}
+ 
+ for( $i=0; $i -lt $components.Count; $i++ )
+ {
+  if ($components.item($i).name -eq "UpdateNotify.Object.2")
+  {$components.Remove($i)
+   $components.SaveChanges()
+  }
+ }
+  $components = $applications.GetCollection("Components",$application.key) 
+  $components.Populate()
+  $componentscount= $components.Count
+  if($componentscount -eq 1)
+ { cls 
+  write-host $true}
+  }
+}
+}
+Set-OfficeC2RCom
